c4e6b17f9cd24d71a59aff84df1cc3548c96d29b
Carlos Miguel Trzaskos <trzaskos.carlos@gmail.com>
2023-12-05 15:05:04 -0300

    "desafio" site que mostra as horas e muda as imagens com o horario

---
+++ aula12ex/script.js
@@ -0,0 +1,18 @@
+function carregar(){
+    var data = new Date()
+    var hora = 19//data.getHours()
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if( hora >= 0 && hora < 12){
+        img.src='imagens/pexels-ravi-kant-14888670.jpg'
+        
+    } else if(hora >=12 && hora <=18){
+        img.src = 'imagens/pexels-george-desipris-858241.jpg'
+        document.body.style.background ='orange'
+    } else {
+        img.src = 'imagens/pexels-piccinng-3052361.jpg'
+        document.body.style.background ='gray'
+    }
+}
