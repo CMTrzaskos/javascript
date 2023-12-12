74130ebc9264922df4d6c9dd0911ef5eed6577eb
Carlos Miguel Trzaskos <trzaskos.carlos@gmail.com>
2023-12-12 19:57:10 -0300

    aaaaaaaaaaaa

---
+++ aula14/ex1/script.js
@@ -0,0 +1,9 @@
+function verificar(){
+    var c =document.getElementById('n1')
+    var entre = document.getElementById('n2')
+    var fim = document.getElementById('n3')
+
+    while (c>7){
+        
+    }
+}
